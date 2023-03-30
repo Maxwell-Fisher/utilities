5932ead01608a4a4cc1134435316e5e8c0aee988
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2023-03-30 15:30:22 -0600

    Created Windows version of GetUSBInfo

---
+++ Win/getUSBInfo.bat
@@ -0,0 +1,11 @@
+@pnputil /enum-devices /connected /class USB > "%tmp%\usbtmp1"
+:checkLoop
+@ping 127.1 -n 2 > nul
+@pnputil /enum-devices /connected /class USB > "%tmp%\usbtmp2"
+@fc /LB1000 "%tmp%\usbtmp1" "%tmp%\usbtmp2" > nul && goto checkLoop
+@findstr /bevg:"%tmp%\usbtmp1" "%tmp%\usbtmp2" | findstr ":"
+@findstr /bevg:"%tmp%\usbtmp2" "%tmp%\usbtmp1" | findstr ":"
+@del "%tmp%\usbtmp1"
+@del "%tmp%\usbtmp2"
+@echo.
+
